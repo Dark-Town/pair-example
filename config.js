1c4f654ea5b5732ef5a75333de08833c91e85e27
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-05-30 02:11:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
 module.exports = {
-  PREFIX: process.env.PREFIX || "ABTECH+",
-  EMAIL: process.env.EMAIL || "abrahamdw882@gmail.com",
-  PASS: process.env.PASS || "#abbotdevi1",
+  PREFIX: process.env.PREFIX || "Tcroneb+",
+  EMAIL: process.env.EMAIL || "thelastcroneb@gmail.com",
+  PASS: process.env.PASS || "Tcroneb/2025",
   MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
   IMAGE: process.env.IMAGE || "https://i.ibb.co/Mh87DFd/633c524b-3e4e-40ed-8ad8-485b36800d5d.jpg",
 }
